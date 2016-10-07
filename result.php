685de4460810fa824f61468d72440d3287c47baf
Messant <messant.d@gmail.com>
2016-10-07 12:00:14 +0200

    Done

--- result.php
+++ result.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 $ville = $_POST['ville'];
 $request = 'http://api.openweathermap.org/data/2.5/weather?q='.$ville.'&APPID=2093ff5449c0cd4294aadbf8c0355fdd';
 $response  = file_get_contents($request);
